9778d8ff6c6d266407c1671df2ad2654cd800367
Andy Lindeman <alindeman@gmail.com>
2012-11-18 11:13:52 -0500

    Only require railtie if Rails is involved

--- lib/rails4_upgrade.rb
+++ lib/rails4_upgrade.rb
@@ -1,6 +1,6 @@
 require "rails4_upgrade/version"
 require "rails4_upgrade/gemfile"
-require "rails4_upgrade/railtie"
+require "rails4_upgrade/railtie" if defined?(::Rails)
 require "rails4_upgrade/output"
 
 module Rails4Upgrade
