20b52d8ffde284d9e157d05adce3a1311d02b867
Andy Lindeman <alindeman@gmail.com>
2013-05-24 22:14:12 -0400

    Ignores coffee-rails and sass-rails
    * These gems will be upgraded as a part of upgrading Rails

--- lib/rails4_upgrade/gem.rb
+++ lib/rails4_upgrade/gem.rb
@@ -4,7 +4,7 @@ require "rails4_upgrade/incompatibility"
 module Rails4Upgrade
   class Gem < Struct.new(:name, :version, :dependencies)
     RAILS_GEMS = Set.new(
-      %w(actionmailer actionpack activemodel activerecord activesupport railties rails)
+      %w(actionmailer actionpack activemodel activerecord activesupport railties rails coffee-rails sass-rails)
     ).freeze
 
     def initialize(name, version, dependencies)
